615dbdeec22c640b978b4bd4d1131f6cab72a7c7
260156 <1461065472@qq.com>
2021-04-02 17:40:49 +0800

    feat(util.js): return svg captcha

--- app/controller/util.js
+++ app/controller/util.js
@@ -5,9 +5,17 @@ const Controller = require('egg').Controller
 
 class UtilController extends Controller {
   async captcha() {
-    const captcha = svgCaptcha.create()
-    console.log(captcha)
-    this.ctx.body = 'captcha'
+    const captcha = svgCaptcha.create({
+      size: 4,
+      fontSize: 50,
+      width: 100,
+      height: 40,
+      noise: 3
+    })
+    
+    this.ctx.session.captcha = captcha.text
+    this.ctx.response.type = "image/svg+xml"
+    this.ctx.body = captcha.data
   }
 }
 
